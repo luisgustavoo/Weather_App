58226b359b8c1d6704e1b06128c2c08f204fe275
Luis Gustavo <luisgustavodevbr@gmail.com>
2023-11-07 08:20:35 -0300

    Refactor Service.swift: remove unused variable
    The `Service` class in `Service.swift` had an unused variable called `session`. This commit removes the unused variable to improve code readability and maintainability.

--- WeatherApp/Service.swift
+++ WeatherApp/Service.swift
@@ -17,7 +17,6 @@ class Service {
     private let baseUrl: String = "https://api.openweathermap.org/data/3.0/onecall"
     private let apiKey: String = "fdf58a70845efcd91ea2a0c238b6b3ae"
     private let session = URLSession.shared
-    
     func fetchData(city: City, completion: @escaping (String) -> Void) -> Void{
         let urlString = "\(baseUrl)?lat=\(city.lat)&lon=\(city.long)&appid=\(apiKey)"
         guard let url = URL(string: urlString) else {return}
